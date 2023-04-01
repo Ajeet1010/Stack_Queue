d47cb9ec61710259bf0249a3a4335a9ce9f8fa88
Ajeet <ajeetpal101999@gmail.com>
2023-04-02 03:59:21 +0530

    [Ajeet] Stack & Queue problems.

---
+++ Stack_Queue/Program.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Stack_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****  STACK & QUEUE Problems");
+        }
+    }
+}
